2bc703d1185e13696da762afa463598478d961c5
Kotorbmt <70278190+Kotorbmt@users.noreply.github.com>
2020-08-27 23:11:24 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
+<!-- php タグの前に書く方法 -->
 <!DOCTYPE html>
-<html>
+<html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <title>Café Progate</title>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
-  <link href='https://fonts.googleapis.com/css?family=Pacifico|Lato' rel='stylesheet' type='text/css'>
+<?php
+
+// echo で出力する方法
+echo '<meta charset="UTF-8">';
+
+// 文字列リテラルとして書く方法（このままでは出力されません）
+$title = 'php で HTML を書く方法';
+
+// 文字列操作でつなげることも出来ます
+echo "<title>{$title}</title>";
+
+// 一時的に php タグから脱出する方法
+?>
 </head>
 <body>
-  <div class="menu-wrapper container">
-    <h1 class="logo">Café Progate</h1>
-  </div>
+<?php
+
+// ヒアドキュメント で HTML を書く方法
+$body = <<<HTML
+Hello
+World!
+<script>
+console.log('不正な処理');
+</script>
+HTML;
+
+// 文字列を関数で加工して表示する方法
+echo nl2br(htmlspecialchars($body));
+
+// php タグの後に書く方法
+?>
 </body>
 </html>
