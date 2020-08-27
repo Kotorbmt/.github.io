1dd48b1245a20d6b705dbd3b9e1b27504c01a238
Kotorbmt <70278190+Kotorbmt@users.noreply.github.com>
2020-08-27 23:09:25 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,46 +1,3 @@
-<?php
-// date.phpのデータ
-
-$juice = new Menu('JUICE', 600, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/juice.png');
-$coffee = new Menu('COFFEE', 500, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/coffee.png');
-$curry = new Menu('CURRY', 900, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/curry.png');
-$pasta = new Menu('PASTA', 1200, 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/php/pasta.png');
-
-$menus = array($juice, $coffee, $curry, $pasta);
-
-// date.phpのデータ、ここまで
-
-
-// menu.phpのデータ
-
-class Menu {
-  public $name;
-  public $price;
-  public $image;
-  
-  public function __construct($name, $price, $image) {
-    $this->name = $name;
-    $this->price = $price;
-    $this->image = $image;
-  }
-  
-  public function hello() {
-    echo '私は'.$this->name.'です';
-  }
-  
-  public function getTaxIncludedPrice(){
-    return floor($this->price*1.08);
-  }
-  
-}
-
-// menu.phpのデータ、ここまで
-
-?>
-
-
-
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -52,15 +9,6 @@ class Menu {
 <body>
   <div class="menu-wrapper container">
     <h1 class="logo">Café Progate</h1>
-    <div class="menu-items">
-      <?php foreach ($menus as $menu): ?>
-        <div class="menu-item">
-          <img src="<?php echo $menu->image ?>" class="menu-item-image">
-          <h3 class="menu-item-name"><?php echo $menu->name ?></h3>
-          <p class="price">¥<?php echo $menu->getTaxIncludedPrice() ?>（税込）</p>
-        </div>
-      <?php endforeach ?>
-    </div>
   </div>
 </body>
 </html>
